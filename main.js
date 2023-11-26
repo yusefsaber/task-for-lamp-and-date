378647c6a59fa29729594ce0006f9d4cea9cf7ab
yusefsaber <152041205+yusefsaber@users.noreply.github.com>
2023-11-26 03:07:33 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var counter = 0;
+function dataInc(){
+    counter++;
+    document.getElementById('yusef').innerHTML = counter;
+}
+function dataDec(){
+    counter--;
+    document.getElementById('yusef').innerHTML = counter;
+}
